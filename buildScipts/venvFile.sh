37c8a63626be65018d9d07413cbf3aaa1228d91d
Boyd Gordon <49822283+PyOfile@users.noreply.github.com>
2019-08-04 13:26:36 -0700

    Create venvFile.sh

---
+++ buildScipts/venvFile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# builds a file system and blank docs
+
+sudo mkdir templates static views viewmodels date && sudo mkdir templates/index templates/about templates/pay templates/form templates/arch && sudo mkdir static/css static/js static/img && sudo touch app.py vi_mod.py req.txt
