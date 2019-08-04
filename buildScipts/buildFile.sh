83dd39ded871136897198052f2085623298ccd21
Boyd Gordon <49822283+PyOfile@users.noreply.github.com>
2019-08-04 14:36:10 -0700

    Update buildFile.sh

--- buildScipts/buildFile.sh
+++ buildScipts/buildFile.sh
@@ -2,4 +2,4 @@
 
 # builds a file system and blank docs
 
-sudo mkdir templates static views viewmodels date && sudo mkdir templates/index templates/about templates/pay templates/form templates/arch && sudo mkdir static/css static/js static/img && sudo touch app.py vi_mod.py req.txt
+sudo mkdir templates static views viewmodels data && sudo mkdir templates/index templates/about templates/pay templates/form templates/arch && sudo mkdir static/css static/js static/img && sudo touch app.py vi_mod.py req.txt
