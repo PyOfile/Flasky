82b930e738b551aa65be12b74a155a7532f142a2
Boyd Gordon <49822283+PyOfile@users.noreply.github.com>
2019-08-04 16:54:04 -0700

    Update buildVenv.sh

--- buildScipts/buildVenv.sh
+++ buildScipts/buildVenv.sh
@@ -2,4 +2,4 @@
 
 #updates &upgrades debian system and installs the venv. You can change the name at the replacing venv to the name you like.
 
-sudo apt update && sudo apt upgrade -y && sudo apt install python3-pip && sudo pip3 install virtualenv && sudo virtualenv venv
+sudo apt update && sudo apt upgrade -y && sudo apt install python3-pip && sudo -H pip3 install virtualenv && sudo virtualenv venv
