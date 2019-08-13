45844d77ac2b82f6168414ca73d918d48f19da59
Boyd Gordon <49822283+PyOfile@users.noreply.github.com>
2019-08-13 15:40:26 -0700

    Update dat.py

--- dat.py
+++ dat.py
@@ -10,7 +10,7 @@ class Drop(SqlAlchemyBase):
     
     id = sa.Column(sa.Integer, primary_key=True)
     email = sa.Column(sa.String, index=True, unique=True, nullable=True)
-    created_date = sa.Column(sa.DateTime, index=True)
+    created_date = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
 
 
 def __rep__(self):
