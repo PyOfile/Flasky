f324042753f2937c6b4f5fa41bbd6efd0008b556
Boyd Gordon <49822283+PyOfile@users.noreply.github.com>
2019-08-14 06:19:50 -0700

    Add files via upload
    database id improvements.

--- dat.py
+++ dat.py
@@ -1,9 +1,9 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
-import datetime 
+import datetime
 from mod import SqlAlchemyBase
 """
-First time sqlalchemy 08/13/19
+First time sqlalchemy 08/14/19
 """
 class Drop(SqlAlchemyBase):
     __tablename__ = "drop"
@@ -11,6 +11,10 @@ class Drop(SqlAlchemyBase):
     id = sa.Column(sa.Integer, primary_key=True)
     email = sa.Column(sa.String, index=True, unique=True, nullable=True)
     created_date = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
+
+
+    package_id = sa.Column(sa.String, sa.ForeignKey("drop.id"))
+    drop = orm.relation('Drop')
 
 
 def __rep__(self):
