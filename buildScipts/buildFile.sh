ea0a6bcb5630283f4e7a4cc8fc2cedbc3db61f35
Boyd Gordon <49822283+PyOfile@users.noreply.github.com>
2019-08-22 06:20:07 -0700

    Delete buildFile.sh
    will be put in bin file

--- buildScipts/buildFile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# builds a file system
-
-sudo mkdir templates static views db && sudo mkdir templates/index templates/about templates/pay templates/form templates/arch templates/cms && sudo mkdir static/css static/js static/img
